44d37f8658243e1f0d98482dc85107e19e067327
yasuori83-10 <126753112+yasuori@users.noreply.github.com>
2023-03-15 22:10:17 +0900

    postsコントローラー作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,4 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
-
-  def new
-  end
-
-  def create
-    Post.create(memo: params[:memo])
-  end
+ def index
+ end
 end
